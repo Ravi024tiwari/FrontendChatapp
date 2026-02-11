7e5e176a2e440c63dd7e42fad96b1c598c0dae59
Ravi Tiwari <raviashoktiwari9559@gmail.com>
2026-02-11 22:09:14 +0530

    again modify the sidebar user api

--- src/components/common/Conversation.jsx
+++ src/components/common/Conversation.jsx
@@ -15,7 +15,7 @@ const Conversations = () => {
         const getConversations = async () => {
             setLoading(true);
             try {
-                const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/VITE_BACKEND_URL`, {
+                const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/sidebar/user`, {
                     withCredentials: true,
                 });
                 
