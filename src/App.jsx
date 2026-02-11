f67bb37f4c45666af946063d6201492295f2c0eb
Ravi Tiwari <raviashoktiwari9559@gmail.com>
2026-02-11 22:28:41 +0530

    updating the path route

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import "./index.css";
 
 // Components & Pages
 import Navbar from "./components/common/NavBar.jsx";
-import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
+import Signup from "./pages/SignUp.jsx"
 import Home from "./pages/Home.jsx";
 import ProfileView from "./pages/ProfileView.jsx";
 import UpdateProfile from "./pages/UpdateProfile.jsx";
